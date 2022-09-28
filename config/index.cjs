6d98e83c653587ada492b43695f415aad2f20df0
longfeng-lai <114070756+longfeng-lai@users.noreply.github.com>
2022-09-28 11:05:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,4 +1,5 @@
-const const config = {
+/* eslint-disable */
+const USER_CONFIG = {
   /**
    * 公众号配置
    */
@@ -13,12 +14,217 @@ const const config = {
   // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: '520b62e39c0473d13036e332c72ca1b3',
 
- 
-  // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-  id: 'otCwU57e9Kefns7o3ec2iC6qyK9s',
-  // 你想对他发送的模板消息的模板ID
-  useTemplateId: 'nsy8Ij4eX14r5f-vJRzByT4CpgjG9HaQDCSobP604VY',
-     
+  // 是否给文字设置多彩颜色, 和emoji不兼容
+  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
+  IS_SHOW_COLOR: true,
+
+  // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句
+    CIBA: true,
+    // 每日一言
+    oneTalk: false,
+    // 土味情话(彩虹屁)
+    earthyLoveWords: false,
+    // 朋友圈文案
+    momentCopyrighting: false,
+    // 毒鸡汤
+    poisonChickenSoup: false,
+    // 古诗古文
+    poetry: false,
+
+    /** 星座运势 */
+    horoscope: false,
+
+    /** 生日消息和节日消息 */
+    birthdayMessage: true,
+
+    // 学生课表
+    courseSchedule: false,
+  },
+
+  /** 每日一言 */
+
+  // 每日一言的内容类型
+  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+  LITERARY_PREFERENCE: '',
+
+  /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
+   */
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'otCwU57e9Kefns7o3ec2iC6qyK9s',
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: 'nsy8Ij4eX14r5f-vJRzByT4CpgjG9HaQDCSobP604VY',
+      // 所在省份
+      province: '湖南',
+      // 所在城市
+      city: '长沙',
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '04-26',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: 'https://wangxinleo.cn',
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '2003', date: '03-25',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '憨憨', year: '2002', date: '08-14',
+        },
+        {
+          type: '节日', name: '在一起的日子', year: '2020', date: '07-31',
+        }
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2020-07-31' },
+      ],
+      // 课程表相关配置
+      // 如果courseSchedule不存在或者为空（null）则认为没有课程
+      // 如果courseSchedule是一个数组，则认为不区分单双周，直接填写星期几对应的课表数据即可
+      // 如果courseSchedule是一个对象（如下面所示）
+      courseSchedule: {
+        // 单双周的基准
+        benchmark: {
+          // 这里设置一个日期，用来作为判断课表是否单双周的依据
+          date: '2022-09-23',
+          // 该日期是否为单周
+          isOdd: false
+        },
+        // 课表
+        courses: {
+          // 单周课表
+          // 从星期一到星期日（星期六和星期日的课表数组可不填写）
+          odd: [
+            // 例子，周一的课表
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周二
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周三
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周四
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周五
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周六
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ],
+            // 周日
+            [
+              '08:00-09:35 高等数学',
+              '09:50-11:25 高等物理'
+            ]
+          ],
+          // 双周课表
+          even: [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+          ]
+        }
+      },
+    },
+    {
+      name: '老婆1',
+      id: '',
+      useTemplateId: '',
+      province: '',
+      city: '',
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: 'https://wangxinleo.cn',
+      festivals: [],
+      customizedDateList: [],
+      courseSchedule: null
+    },
+    {
+      name: '老婆2',
+      id: '',
+      useTemplateId: '',
+      province: '',
+      city: '',
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: 'https://wangxinleo.cn',
+      festivals: [],
+      customizedDateList: [],
+      courseSchedule: null
+    },
+    {
+      name: '老婆3',
+      id: '',
+      useTemplateId: '',
+      province: '',
+      city: '',
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: 'https://wangxinleo.cn',
+      festivals: [],
+      customizedDateList: [],
+      courseSchedule: null
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
+
+  /**
+   * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
+   */
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '',
+
+  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
+  CALLBACK_USERS: [
+    {
+      // 一般都填自己
+      name: '自己',
+      // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'gh_1bf22ea498cf ',
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
+
   /**
    * 信息配置
    */
@@ -52,9 +258,9 @@ const const config = {
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
     {
-      type: '生日', name: '宝贝', year: '2003', date: '03-25', isShowAge: true,
+      type: '生日', name: '老婆', year: '2003', date: '03-25', isShowAge: true,
     },
-  
+
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
     {
       type: '生日', name: '憨憨', year: '2002', date: '08-14', isShowAge: true,
@@ -71,7 +277,7 @@ const const config = {
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 0,
+  FESTIVALS_LIMIT: 3,
 
   /** 日期相关 */
 
@@ -86,11 +292,30 @@ const const config = {
     // ...
   ],
 
-   /** 每日一言 */
-  
-  //可以填写【动画，漫画，游戏，小说，原创，网络，其他】：随机则填写""
-  LITEPARY_PREFERENCE:""
+  /** 插槽 */
+
+  /** 你可以在这里写超多的你想显示的内容了！
+   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
+   * */
+  SLOT_LIST: [
+    // 这样配置的话，就会每次发送这句话
+    { keyword: 'encourage_oneself', contents: '你主要的问题在于读书太少而想得太多' },
+    // 这样配置的话，就会每次随机选一句话发送
+    {
+      keyword: 'lover_prattle',
+      contents: [
+        '因为太喜欢你，所以看谁都像是情敌。',
+        '申请成为你爱里的永久居民。',
+        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
+        '遇见你，就好像捡到了100斤的运气'
+      ],
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
+  // 默认的课表配置
+  COURSE_SCHEDULE: null
 }
-  
 
-
+module.exports = USER_CONFIG
